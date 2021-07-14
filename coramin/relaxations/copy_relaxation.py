22187e5f9e1631867c29f981ff6dc035341bd23d
Dillard Robertson <drobertson40@gatech.edu>
2021-07-13 22:06:59 -0400

    BAD! Impliment copying code for egret SOCEdgeCuts relaxation.

--- coramin/relaxations/copy_relaxation.py
+++ coramin/relaxations/copy_relaxation.py
@@ -4,6 +4,7 @@ from .univariate import PWXSquaredRelaxationData, PWUnivariateRelaxationData, PW
 from .univariate import PWXSquaredRelaxation, PWUnivariateRelaxation, PWArctanRelaxation, \
     PWCosRelaxation, PWSinRelaxation
 from .multivariate import MultivariateRelaxationData, MultivariateRelaxation
+from egret.models.ac_relaxations import SOCEdgeCutsData, SOCEdgeCuts
 from pyomo.core.expr.visitor import replace_expressions
 from coramin.utils.coramin_enums import FunctionShape
 
@@ -96,6 +97,20 @@ def copy_relaxation_with_local_data(rel, old_var_to_new_var_map):
         new_rel = MultivariateRelaxation(concrete=True)
         new_rel.set_input(aux_var=new_aux_var, shape=shape, f_x_expr=new_f_x_expr,
                           use_linear_relaxation=rel.use_linear_relaxation)
+    elif isinstance(rel, SOCEdgeCutsData):
+        rhs_vars = rel.get_rhs_vars()
+        old_c = rhs_vars[0]
+        old_s = rhs_vars[1]
+        old_vmsq_1 = rhs_vars[2]
+        old_vmsq_2 = rhs_vars[3]
+        new_c = old_var_to_new_var_map[id(old_c)]
+        new_s = old_var_to_new_var_map[id(old_s)]
+        new_vmsq_1 = old_var_to_new_var_map[id(old_vmsq_1)]
+        new_vmsq_2 = old_var_to_new_var_map[id(old_vmsq_2)]
+        new_rel = SOCEdgeCuts(concrete=True)
+        #TODO: do we need to copy persistant solvers?
+        new_rel.set_input(c=new_c, s=new_s,
+                          vmsq_1=new_vmsq_1, vmsq_2=new_vmsq_2)
     else:
         raise ValueError('Unrecognized relaxation: {0}'.format(str(type(rel))))
 
